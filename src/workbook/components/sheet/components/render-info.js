ebc04ac7291cea669e7a6ac7313792a0aacae43d
hancong03 <hancong03@baidu.com>
2015-07-14 15:07:09 +0800

    升级renderinfo，增加超链接数据
    Signed-off-by: hancong03 <hancong03@baidu.com>

--- src/workbook/components/sheet/components/render-info.js
+++ src/workbook/components/sheet/components/render-info.js
@@ -16,10 +16,53 @@ define(function (require) {
         },
 
         getRenderInfo: function (rows, cols) {
+            var otherInfo = this.__getOtherInfo(rows, cols);
             return {
                 styles: this.getModule('StyleCache').getRenderStyle(rows, cols),
-                comments: this.getModule('Comment').getComments(rows, cols)
+                comments: otherInfo.comments,
+                hyperlinks: otherInfo.hyperlinks
             };
+        },
+
+        __getOtherInfo: function (rows, cols) {
+            var result = {
+                comments: {},
+                hyperlinks: {}
+            };
+            var row;
+            var col;
+            var key;
+
+            var sheetData = this.getActiveSheet();
+            var rowsData = sheetData.cell.rows;
+            var comments = sheetData.comments;
+            var hyperlinks = sheetData.hyperlinks;
+
+            var current;
+
+            for (var i = 0, len = rows.length; i < len; i++) {
+                row = rows[i];
+                for (var j = 0, jlen = cols.length; j < jlen; j++) {
+                    col = cols[j];
+
+                    if (!rowsData[row] || !rowsData[row].cells || !rowsData[row].cells[col]) {
+                        continue;
+                    }
+
+                    current = rowsData[row].cells[col];
+                    key = row + ',' + col;
+
+                    if ($$.isDefined(current.comment)) {
+                        result.comments[key] = comments[current.comment];
+                    }
+
+                    if ($$.isDefined(current.hyperlink)) {
+                        result.hyperlinks[key] = hyperlinks[current.hyperlink];
+                    }
+                }
+            }
+
+            return result;
         }
     });
 });
