9a336d3762a7ba7319a9c90f76f4081d1307400b
hancong03 <hancong03@baidu.com>
2015-08-05 22:09:02 +0800

    增加formula类型
    Signed-off-by: hancong03 <hancong03@baidu.com>

--- src/workbook/definition/vtype.js
+++ src/workbook/definition/vtype.js
@@ -5,15 +5,8 @@
 
 define({
     NUMBER: 'n',
-    //NUMBER_STRING: 'ns',
-    //CURRENCY: 'c',
-    //ACCOUNTANT: 'a',
-    //TIME: 't',
-    //DATETIME: 'dt',
-    //PERCENTAGE: 'p',
-    //FRACTION: 'f',
-    //SCIENTIFIC: 'sc',
     TEXT: 's',
     LOGICAL: 'l',
-    ERROR: 'e'
+    ERROR: 'e',
+    FORMULA: 'f'
 });
